f68ec63e038b2513f25985d365791a76f90b54d7
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2019-07-29 19:51:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from classes import Person, Auto, Aircraft
+
+tom = Person("Tom")
+tom.display_info()
+
+bmw = Auto("BMW")
+bmw.move(65)
+
+Messerschmitt = Aircraft("Messerschmitt")
+Messerschmitt.move(1050)
